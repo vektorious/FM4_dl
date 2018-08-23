90c03e386aa1edb2fae9d34420b05067c5705786
Alexander Kutschera <alexander.kutschera@gmail.com>
2018-08-23 22:55:23 +0200

    added Dropbox support

--- script/fm4_dl.sh
+++ script/fm4_dl.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-#SHOW_URL="$(sudo python fm4.py -s '4HOPWed')" #insert you favourite show tag here
+SHOW_TAG = '4HOPWed' #insert you favourite show tag here
+#SHOW_URL="$(sudo python fm4.py -s ${SHOW_TAG})"
 SHOW_URL="https://www.abendzeitung-muenchen.de/media.media.43b9b948-2982-473a-8401-6ae566a63557.original1024.jpg"
 DATE=`date +%Y-%m-%d`
 
-wget -O ../downloads/HOP/${DATE}_HOP.mp3 ${SHOW_URL}
+wget -O ../downloads/${SHOW_TAG}/${DATE}_${SHOW_TAG}.mp3 ${SHOW_URL}
+
+../Dropbox-Uploader/dropbox_uploader.sh upload ../downloads/${SHOW_TAG}/${DATE}_${SHOW_TAG}.mp3 /${SHOW_TAG}/2018-08-23_${SHOW_TAG}.mp3

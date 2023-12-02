c5c2ed40485772a8b7c8ebb93e026c9ed5de3bcf
Thomas <tvhdev@vonhaugwitz-softwaresolutions.de>
2023-12-02 13:03:08 +0100

    specify show tags, download multiple shows at once
    enhanced script to specify show tags in order to download multiple shows at once. Also introduced variable STORAGE to specify storage location

--- script/fm4_dl.sh
+++ script/fm4_dl.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" #set wd to file location
-SHOW_TAG='4HOPWed' #insert you favourite show tag here
-SHOW_URL="$(python fm4.py -s ${SHOW_TAG}| tr -d '[],')" #call python script to get the stream URL
-
-mkdir -p ../downloads/${SHOW_TAG} #creates show directory if it doesn't exist
-
+SHOW_TAGS=(4UL 4DKM 4DD 4LB)  #insert you favourite show tags here
+STORAGE=/music
 i=1
-for URL in $SHOW_URL; do
+for SHOW_TAG in "${SHOW_TAGS[@]}"; do
+  echo $SHOW_TAG
+  URL="$(python fm4.py -s ${SHOW_TAG}| tr -d '[],')" #call python script to get the stream URL
+  mkdir -p ${STORAGE}/${SHOW_TAG} #creates show directory if it doesn't exist
+  echo $URL
   URL="${URL%\'}" #removes the starting quote
   URL="${URL#\'}" #removes the last quote
   DATE="${URL#*id=}"
   echo $DATE
   DATE="${DATE%%_*}"
-  echo $DATE
-  wget -O ../downloads/${SHOW_TAG}/${DATE}_${SHOW_TAG}_${i}.mp3 ${URL} #download show
+  wget -O ${STORAGE}/${SHOW_TAG}/${DATE}_${SHOW_TAG}_${i}.mp3 ${URL} #download show
   #../Dropbox-Uploader/dropbox_uploader.sh upload ../downloads/${SHOW_TAG}/${DATE}_${SHOW_TAG}_${i}.mp3 /${SHOW_TAG}/2018-08-23_${SHOW_TAG}.mp3
   #upload to Dropbox folder: remove the line above if you don't want to use the dropbox uploader
   let "i=i+1"
 done
-
-#

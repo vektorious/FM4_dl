1654aa2ed24e51f3cdf4a325c3832a585b0f3dc0
Thomas <tvhdev@vonhaugwitz-softwaresolutions.de>
2023-12-02 14:27:37 +0100

    skip already downloaded shows

--- script/fm4_dl.sh
+++ script/fm4_dl.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" #set wd to file location
-SHOW_TAGS=(4UL 4DKM 4DD 4LB)  #insert you favourite show tags here
-STORAGE=/music
-i=1
+SHOW_TAGS=(4UL 4DKM 4DD 4LB 4SS)  #insert you favourite show tags here
+STORAGE=/mnt/storage/Musik/FM4/downloads
 for SHOW_TAG in "${SHOW_TAGS[@]}"; do
   URL="$(python fm4.py -s ${SHOW_TAG}| tr -d '[],')" #call python script to get the stream URL
   mkdir -p ${STORAGE}/${SHOW_TAG} #creates show directory if it doesn't exist
-  echo $URL
   URL="${URL%\'}" #removes the starting quote
   URL="${URL#\'}" #removes the last quote
   DATE="${URL#*id=}"
   DATE="${DATE%%_*}"
-  wget -O ${STORAGE}/${SHOW_TAG}/${DATE}_${SHOW_TAG}_${i}.mp3 ${URL} #download show
-  #../Dropbox-Uploader/dropbox_uploader.sh upload ../downloads/${SHOW_TAG}/${DATE}_${SHOW_TAG}_${i}.mp3 /${SHOW_TAG}/2018-08-23_${SHOW_TAG}.mp3
-  #upload to Dropbox folder: remove the line above if you don't want to use the dropbox uploader
-  let "i=i+1"
+  FILENAME="${STORAGE}/${SHOW_TAG}/${DATE}_${SHOW_TAG}.mp3"
+  if [ ! -f ${FILENAME} ]
+  then
+  wget -O ${FILENAME} ${URL} #download show
+  # upload to cloud ? use rclone! 
+  else
+  echo "skipping file ${FILENAME}, it does already exist"
+  fi
 done
